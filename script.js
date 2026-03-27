521a691181dee4d81278bfa9c6c412df8485433e
João Vitor Alexandre Ribeiro <jvaribeiro.dev@gmail.com>
2026-03-27 09:30:34 -0300

    Deixando o html mais semântica

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 
 mobileMenuButton.addEventListener('click', () => {
+    const isExpanded = mobileMenu.classList.contains('hidden');
     mobileMenu.classList.toggle('hidden');
+    mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
 });
 
 // Script para fechar menu mobile ao clicar em um link
@@ -11,5 +13,6 @@ const navLinks = document.querySelectorAll('#mobile-menu a');
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         mobileMenu.classList.add('hidden');
+        mobileMenuButton.setAttribute('aria-expanded', 'false');
     });
 });
